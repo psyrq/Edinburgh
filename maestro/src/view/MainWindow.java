7ad9d1828b92a828010c3b14fe69e4f1b19ddbf6
Ruofei Qian <ruofei.qian@cienet.com.cn>
2018-12-11 08:56:24 +0800

    update

--- maestro/src/view/MainWindow.java
+++ maestro/src/view/MainWindow.java
@@ -5,6 +5,12 @@ import javax.swing.*;
 public class MainWindow {
 	
 	private static JFrame jFrame;
+
+	private static JPanel jPanel;
+
+	private static JLable hotKey1,hotKey2,hotKey3,hotKey4,hotKey5,hotKey6;
+
+	private static JTextField setKey1,setKey1,setKey1,setKey1,setKey1,setKey1,
 	
 	public MainWindow() {
 		createMainUI();
@@ -15,13 +21,19 @@ public class MainWindow {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		
 		jFrame = new JFrame("Maestro window");
+		jFrame.setSize(350,200);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel jLable = new JLabel("set hotkey");
-		jFrame.getContentPane().add(jLable);
+		jPanel = new JPanel();
 		
-		jFrame.pack();
+		jFrame.add(jPanel);
+		//jFrame.pack();
 		jFrame.setVisible(true);
+	}
+
+	private setComponents(JPanel jPanel) {
+		JLable subTitle = new JLable("set hotkey");
+		
 	}
 	
 	public JFrame getJFrame() {
